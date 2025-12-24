5af7eee46800596ee1a94b09f923651b440b8f52
Sheetal <kolars293@gmail.com>
2025-12-24 19:10:05 +0530

    added new file

--- src/test/java/com/ui/tests/Logintest.java
+++ src/test/java/com/ui/tests/Logintest.java
@@ -13,21 +13,21 @@ import com.ui.pojo.User;
 @Listeners(com.ui.listeners.TestListener.class)
 public class Logintest extends TestBase{
 
-//    @Test(description = " Verifies if the valid user is able to login into the application", groups = { "e2e",
-//			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
-//	public void loginTest(User user) {
-//
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Sheetal Kolar");
-//	}
-//
-//	@Test(description = " Verifies if the valid user is able to login into the application", groups = { "e2e",
-//			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
-//	public void loginCSVTest(User user) {
-//
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Sheetal Kolar");
-//	}
+    @Test(description = " Verifies if the valid user is able to login into the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
+	public void loginTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Sheetal Kolar");
+	}
+
+	@Test(description = " Verifies if the valid user is able to login into the application", groups = { "e2e",
+			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Sheetal Kolar");
+	}
 
 	@Test(description = " Verifies if the valid user is able to login into the application", groups = { "e2e",
 			"sanity" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
