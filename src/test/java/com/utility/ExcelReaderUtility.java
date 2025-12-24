f4f8cfc0862b193eeaf8e1dad040a6ceb1252b53
Sheetal <kolars293@gmail.com>
2025-12-25 02:30:19 +0530

    Updated the Paths

--- src/test/java/com/utility/ExcelReaderUtility.java
+++ src/test/java/com/utility/ExcelReaderUtility.java
@@ -18,7 +18,7 @@ public class ExcelReaderUtility {
 
 	public static Iterator<User> readExcelFile(String fileName) {
 
-		File xlsxFile = new File(System.getProperty("user.dir") + "//testData//" + fileName);
+		File xlsxFile = new File(System.getProperty("user.dir") + "/testData/" + fileName);
 		// XLSX File
 		XSSFWorkbook xssfWorkbook = null;
 		Row row;
